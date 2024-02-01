9fc61692790bf756b271242df69864a65632305a
Angie <liya0706s@gmail.com>
2024-02-01 11:16:10 +0800

    建置最新文章列表,分頁及按讚功能

--- api/add_que.php
+++ api/add_que.php
@@ -2,32 +2,35 @@
 include_once "db.php";
 
 // 判斷問卷主題是否存在
-if(isset($_POST['subject'])){
+if (isset($_POST['subject'])) {
 
     // 新增主題資料，根據 que 資料表中的欄位，用$Que物件save()方法儲存進資料表
-$Que->save(
-    ['text'=>$_POST['subject'],
-    'subject_id'=>0,
-    'vote'=>0]);
+    $Que->save(
+        [
+            'text' => $_POST['subject'],
+            'subject_id' => 0,
+            'vote' => 0
+        ]
+    );
 
     // 根據剛才新增的主題資料去找到 主題的id
-    $subject_id=$Que->find(['text'=>$_POST['subject']])['id'];
+    $subject_id = $Que->find(['text' => $_POST['subject']])['id'];
 
     // 或是使用max()找到最大的id
     // $subject_id=$Que->max('id');
 }
 
 // 判斷選項是否正確
-if(isset($_POST['option'])){
+if (isset($_POST['option'])) {
     // 使用迴圈來巡訪 $_POST['option'] 陣列
-    foreach($_POST['option'] as $option){
+    foreach ($_POST['option'] as $option) {
         // 新增選項資料 save($array)
-        $Que->save(['text'=>$option,
-                    'subject_id'=>$subject_id,
-                    'vote'=>0]);
+        $Que->save([
+            'text' => $option,
+            'subject_id' => $subject_id,
+            'vote' => 0
+        ]);
     }
 }
 // 導回後台管理頁面
 to("../back.php?do=que");
-
-?>
---
+++ front/news.php
@@ -0,0 +1,105 @@
+<fieldset>
+    <legend>目前位置:首頁 > 最新文章區</legend>
+    <table style="width:95%;margin:auto">
+        <tr>
+            <th width="30%">標題</th>
+            <th width="50%">內容</th>
+            <th></th>
+        </tr>
+        <?php
+        // 製作分頁
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        // 判斷是否有GET網頁傳值的p，有的話就是該頁碼，否則是1
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+
+        // 撈資料限制從開始頁取5筆
+        $rows = $News->all(['sh' => 1], " limit $start,$div");
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td>
+                    <!-- 文章標題游標放上去會是pointer -->
+                    <div class='title' data-id="<?= $row['id']; ?>" style='cursor:pointer'>
+                        <?= $row['title']; ?>
+                    </div>
+                </td>
+                <td>
+                    <!-- 字串中取部分 substr 從0開始取25個字 -->
+                    <div id="s<?= $row['id']; ?>">
+                        <?= mb_substr($row['news'], 0, 25); ?>...
+                    </div>
+                    <div id="a<?= $row['id']; ?>" style="display:none">
+                        <?= $row['news']; ?>
+                    </div>
+                </td>
+                <!-- 根據登入狀態來顯示讚的程式 -->
+                <td>
+                    <?php
+                    // 1.判斷有沒有登入
+                    if (isset($_SESSION['user'])) {
+                        // 2.判斷有沒有按過讚
+                        // 傳遞給count方法的條件參數。這個參數是一個關聯陣列，代表著要搜尋的條件
+                        // count($where='',$other='')
+                        if ($Log->count(['news' => $row['id'], 'user' => $_SESSION['user']]) > 0) {
+                            // 如果大於0，表示在資料庫中找到了符合這些條件的記錄，已經按過讚
+                            echo "<a href='Javascript:good({$row['id']})'>收回讚</a>";
+                        } else {
+                            echo "<a href='Javascript:good({$row['id']})'>讚</a>";
+                        }
+                    }
+                    ?>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <div>
+        <?php
+        if (($now - 1) > 0) {
+            $prev = $now - 1;
+            echo "<a href='?do=news&p=$prev'> < </a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $fontsize = ($now == $i) ? "font-size:20px" : "font-size:16px";
+            echo "<a href='?do=news&p=$i' style='$fontsize'> $i </a>";
+        }
+        if (($now + 1) <= $pages) {
+            $next = $now + 1;
+            echo "<a href='?do=news&p=$next'> > </a>";
+        }
+        ?>
+    </div>
+</fieldset>
+
+<script>
+    /**
+     * 新聞標題添加點擊事件，當點擊某個標題時，會切換該新聞的簡短和完整內容的顯示
+     */
+
+    // 對class title 進行點擊事件註冊
+    $('.title').on('click', (e) => {
+        // e.target是被點擊的DOM元素，$(e.target)將其轉換為jQuery對象
+        // 取得點擊的id，獲取被點擊元素的data-id屬性值
+        // 這個值通常用於識別哪一條新聞被點擊
+        let id = $(e.target).data('id');
+
+        // 將箭頭函數改為傳統的函數表達式如下:
+        // $('.title').on('click',function(){
+        //     let id=$(this).data('id');
+
+
+        // 對id為 s+id,a+id 的元素進行toggle來切顯示與隱藏
+        $(`#s${id},#a${id}`).toggle();
+    });
+
+    function good(news){
+        $.post("./api/good.php",{news},()=>{
+            // 使用重整頁面的方式來更新按讚的結果
+            location.reload();
+        })
+    }
+</script>
